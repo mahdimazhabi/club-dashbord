4f03bad576f9040204ec1b25462326cf00252619
MHD <mahdimazhabi@gmail.com>
2025-03-26 15:54:10 +0330

    finish header

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,6 @@
 /// <reference types="vite-plugin-svgr/client" />
 export { default as NotiIcon } from "@/assets/svg/notif.svg?react";
+export { default as ChevronRightIcon } from "@/assets/svg/chevron-right.svg?react";
+export { default as TierIcon } from "@/assets/svg/tier.svg?react";
+export { default as PointsIcon } from "@/assets/svg/points.svg?react";
+export { default as VoucherIcon } from "@/assets/svg/voucher.svg?react";
--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,11 +1,29 @@
 import { NotiIcon } from "@/assets";
+import { ChevronRightIcon } from "@/assets";
+import { TierIcon, PointsIcon, VoucherIcon } from "@/assets";
 const Header = () => {
+  const DataList = [
+    { id: 1, title: " کد  12  ", icon: <VoucherIcon /> },
+    { id: 2, title: "1.200", icon: <PointsIcon /> },
+    { id: 1, title: "سکه", icon: <TierIcon /> },
+  ];
   return (
     <header>
-      <div className="bg-gradient-to-br from-primary-50 via-primary-40 to-primary-30 p-5">
-        <div className="flex items-center gap-2">
+      <div className="bg-gradient-to-br from-primary-50 via-primary-40 to-primary-30  ">
+        <div className="flex items-center gap-2 p-5">
           <NotiIcon className="text-white font-bold" />
           <h1 className="text-white text-lg">سلام دانیال</h1>
+        </div>
+        <div>
+          <ul className="flex bg-white/15 rounded-t-lg px-4 py-2.5 justify-between text-white ">
+            {DataList.map((item) => (
+              <li key={item.id} className="flex items-center gap-4 ">
+                {item.icon}
+                {item.title}
+                <ChevronRightIcon className="rotate-180" />
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </header>
