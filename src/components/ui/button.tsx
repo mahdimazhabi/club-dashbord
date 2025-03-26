ad99595085fc6069dddca973e6092879a81ce340
MHD <mahdimazhabi@gmail.com>
2025-03-26 14:09:19 +0330

    edit to compornts button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary-50 text-primary-foreground   hover:bg-primary-50/90",
+          "bg-primary-50 text-primary-foreground border    hover:bg-primary-50/90",
         destructive:
           "bg-destructive  text-destructive-foreground  hover:bg-destructive/90",
         outline:
