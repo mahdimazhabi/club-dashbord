393663203167d5dc7e759d7e082884a38c940c8f
MHD <mahdimazhabi@gmail.com>
2025-03-26 14:20:29 +0330

    edit structurs

--- src/routers/routers.tsx
+++ src/routers/routers.tsx
@@ -2,11 +2,16 @@ import { RouteObject } from "react-router-dom";
 import AuthLayout from "@/shared/layout/AuthLayout/AuthLayout";
 import { Navigate } from "react-router-dom";
 import HomeLayout from "@/shared/layout/HomeLayout/HomeLayout.tsx";
+import WellcomeLayout from "@/shared/layout/wellcomeLayout/WellcomeLayout";
 
 export const AllRouters: RouteObject[] = [
   {
     index: true,
-    element: <Navigate to="/home" replace />,
+    element: <Navigate to="/wellcome" replace />,
+  },
+  {
+    path: "/wellcome",
+    element: <WellcomeLayout />,
   },
   {
     path: "/home",
