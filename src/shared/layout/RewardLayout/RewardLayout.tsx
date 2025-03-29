02ce190c12ed238819c224cf46e350001dcf4f5a
MHD <mahdimazhabi@gmail.com>
2025-03-29 12:38:57 +0330

    add new header for page Reward

--- src/shared/layout/RewardLayout/RewardLayout.tsx
+++ src/shared/layout/RewardLayout/RewardLayout.tsx
@@ -1,5 +1,13 @@
+import Navbar from "@/shared/components/Navbar";
+import HeaderReward from "@/shared/components/HeaderReward";
 const RewardLayout = () => {
-  return <div>RewardLayout</div>;
+  return (
+    <div>
+      <HeaderReward />
+      <main className="container"></main>
+      <Navbar />
+    </div>
+  );
 };
 
 export default RewardLayout;
