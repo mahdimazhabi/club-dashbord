d8ae38746fc43a2ba6eef5c1bc6070baac1e69ac
MHD <mahdimazhabi@gmail.com>
2025-03-29 10:09:55 +0330

    edit to profile menu components

--- src/page/profile/components/MneuProfile.tsx
+++ src/page/profile/components/MneuProfile.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const MneuProfile = () => {
   const DataList1 = [
-    { title: "اطلاعات کاربری" },
-    { title: "تاریخچه معاملات" },
-    { title: "کد ارجاع" },
-    { title: "تنظیمات" },
+    { title: "اطلاعات کاربری", path: "information" },
+    { title: "تاریخچه معاملات", path: "" },
+    { title: "کد ارجاع", path: "" },
+    { title: "تنظیمات", path: "" },
   ];
   const DataList2 = [{ title: "درباره ما" }, { title: "سوالات متداول" }];
   return (
@@ -15,7 +15,7 @@ const MneuProfile = () => {
         <h1 className="px-3 text-lg font-bold text-neutral-50 mt-5">عمومی</h1>
         <ul className="mt-2">
           {DataList1.map((item, index) => (
-            <Link key={index} to={""}>
+            <Link key={index} to={item.path}>
               <li className="flex  justify-between text-xs p-3 border-b">
                 {item.title}
                 <ChevronLeft size={20} className="text-neutral-50 " />
